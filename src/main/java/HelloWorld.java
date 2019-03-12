4e71782a455aa96fb748e48047ba3fef0d89738e
vvk30 <vishal.kulkarni303@gmail.com>
2019-03-12 13:15:18 +0530

    Added some paths

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,5 +10,17 @@ public class HelloWorld implements SparkApplication {
 	@Override
 	public void init() {
 		get("/hello", (req, res) -> "Hello World");
+		
+		// matches "GET /hello/foo" and "GET /hello/bar"
+		// request.params(":name") is 'foo' or 'bar'
+		get("/hello/:name", (request, response) -> {
+		    return "Hello: " + request.params(":name");
+		});
+		
+		// matches "GET /say/hello/to/world"
+		// request.splat()[0] is 'hello' and request.splat()[1] 'world'
+		get("/say/*/to/*", (request, response) -> {
+		    return "Number of splat parameters: " + request.splat().length;
+		});
 	}
 }
